5af2f7c42123b269696ce155f5d3945d84d7b450
David Chatterton <dchatter@redhat.com>
2020-03-12 17:37:28 -0400

    Incremental improvements

--- .config/i3/display.sh
+++ .config/i3/display.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-num_of_monitors=`xrandr | awk '/^eDP.* connected|^DP.* connected/{ print $1 }'| wc -l`
-laptop_screen=`xrandr | awk '/^eDP/{ print $1 }'`
-
-mon1=`xrandr | awk '/^DP.* connected/{ print $1 }'| head -1`
-mon2=`xrandr | awk '/^DP.* connected/{ print $1 }'| tail -1`
+xrandr_output=`xrandr`
+num_of_monitors=`echo "$xrandr_output" | grep -E '^eDP.* connected|^DP.* connected' | wc -l`
+laptop_screen=`echo "$xrandr_output" | awk '/^eDP/{ print $1 }'`
 
 # Desk layout
 if [ "$num_of_monitors" -eq 3 ]; then
+  mon1=`echo "$xrandr_output" | awk '/^DP.* connected/{ print $1 }'| head -1`
+  mon2=`echo "$xrandr_output" | awk '/^DP.* connected/{ print $1 }'| tail -1`
   xrandr --output $mon1 --primary --auto --output $mon2 --auto --right-of $mon1 --output $laptop_screen --off
-# Revert to laptop screen
+
+# Default to laptop screen
 else
   xrandr --output $laptop_screen --primary --auto
 fi
