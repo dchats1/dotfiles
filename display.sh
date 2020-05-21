ce1f143cb585d66677a0a88a2c5f5220ad204bcd
David Chatterton <david@davidchatterton.com>
2020-05-21 14:18:05 -0400

    Cleaned up xmonad config and got trayer to work

--- display.sh
+++ display.sh
@@ -4,6 +4,11 @@ num_of_monitors=`xrandr | grep ' connected' | wc -l`
 
 if [ "$num_of_monitors" -eq 3 ]; then # Desk layout
   xrandr --output DP2-1 --primary --auto --output DP2-2 --auto --right-of DP2-1 --output eDP1 --off
+  trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 4 --transparent true --alpha 0 --tint 0x000000 --height 16 --monitor 1 &
+  xmobar -x 0 ~/.xmonad/xmobarrc-dual &
+  xmobar -x 1 ~/.xmonad/xmobarrc &
 else # Revert to laptop
+  trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 4 --transparent true --alpha 0 --tint 0x000000 --height 16 --monitor 0 &
   xrandr --output eDP1 --primary --auto
+  xmobar -x 0 ~/.xmonad/xmobarrc &
 fi
--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,4 @@
 -- IMPORTS
---
 import XMonad
 import Data.Monoid
 import System.Exit
@@ -12,120 +11,67 @@ import XMonad.Util.Run (spawnPipe)
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
+myTerminal = "xfce4-terminal"
 
-myTerminal      = "xfce4-terminal"
-
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
-
--- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
--- Width of the window border in pixels.
---
-myBorderWidth   = 1
+myBorderWidth = 1
 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
-myModMask       = mod4Mask
+myModMask = mod4Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
 
--- Border colors for unfocused and focused windows, respectively.
---
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#ff0000"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
---
+
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- launch a terminal
     [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
-
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenu_run")
-
-    -- launch gmrun
---    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
-
      -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
-
     --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
     -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
-
     -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
-
     -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
-
     -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
-
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
-
     -- Swap the focused window and the master window
     , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
-
     -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
     -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
     -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
-
     -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
-
     -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
     -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
-
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     ]
@@ -212,7 +158,7 @@ myManageHook = composeAll
     [ className =? "Galculator"        --> doFloat
     , className =? "Gimp"           --> doFloat
     , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore ]
+    , resource  =? "trayer"         --> doIgnore ]
 
 ------------------------------------------------------------------------
 -- Event handling
@@ -245,28 +191,25 @@ myLogHook = workspaceNamesPP xmobarPP >>= dynamicLogString >>= xmonadPropLog
 myStartupHook = do
         spawnOnce "~/dotfiles/display.sh &"
         spawnOnce "feh --bg-scale ~/Pictures/Sidelined_LandscapeMt_Cook_New_Zealand.jpg &"
-        spawnOnce "pasystray &"
         spawnOnce "xcompmgr &"
-        spawnOnce "xfce4-power-manager &"
-        spawnOnce "nm-applet &"
         spawnOnce "redshift -Po -t 5800:5500 &"
+        spawnOnce "sleep 2 && nm-applet &"
+        spawnOnce "sleep 2 && xfce4-power-manager &"
+        spawnOnce "sleep 2 && pasystray &"
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
-
 -- Run xmonad with the settings you specify. No need to modify this.
---
+
 main = do
-    xmproc <- spawnPipe "xmobar -x 0 ~/.xmonad/xmobarrc"
-    xmproc <- spawnPipe "xmobar -x 1 ~/.xmonad/xmobarrc"
+--    xmproc <- spawnPipe "xmobar -x 0 ~/.xmonad/xmobarrc"
+--    xmproc <- spawnPipe "xmobar -x 1 ~/.xmonad/xmobarrc"
     xmonad $ docks defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
 -- use the defaults defined in xmonad/XMonad/Config.hs
---
--- No need to modify this.
---
+
 defaults = def {
       -- simple stuff
         terminal           = myTerminal,
